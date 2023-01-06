80135e9b68f4ae0625fd316887134179cf92f2a6
JaeyeongKim <ddi03231@g.skku.edu>
2023-01-06 23:13:46 +0900

    초안Page Routing완료

---
+++ src/pages/PortfolioPage.tsx
@@ -0,0 +1,16 @@
+
+import styled from "styled-components";
+import {useState} from "react";
+
+const PortpolioPageContainer = styled.div`
+    backgorund-color = black;
+`
+
+function PortpolioPage(){
+    return(
+        <PortpolioPageContainer></PortpolioPageContainer>
+    );
+}
+
+
+export default PortpolioPage;
