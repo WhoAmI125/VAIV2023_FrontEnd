ad43d97f5b3c17073f659da402199d4fc69a76d8
WhoAmI125 <whoami125@g.skku.edu>
2023-01-21 15:43:57 +0900

    fixed: update destinatio

--- react_deploy_B.sh
+++ react_deploy_B.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 REPOSITORY= /home/ubuntu
-PROJECT_FOLDER= VAIV2023_FrontEnd
+PROJECT_FOLDER= /VAIV2023_FrontEnd
 
 echo "> swap 메모리 할당 진행"
 sudo dd if=/dev/zero of=/swapfile bs=128M count=16
@@ -17,18 +17,13 @@ echo "> 필요 패키지 다운로드"
 sudo apt install nginx -y
 wait
 ehcho ">curl 설치"
-sudo apt-get install curl
+sudo apt install curl -y
 wait
-curl -o- -L https://yarnpkg.com/install.sh | bash
+curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
 wait
-source ~/.bashrc
+sudo apt-get install nodejs
 wait
-echo "> npm 설치 진행"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-wait
-. ~/.nvm/nvm.sh
-wait
-nvm install node
+npm install --global yarn
 wait
 echo "> node, npm 버전 확인"
 node -v
@@ -36,10 +31,8 @@ npm --v
 echo "> Git Repository 복사"
 git clone https://github.com/VAIV2023/VAIV2023_FrontEnd.git
 wait
-echo "> 디렉토리 이동"
-cd $REPOSITORY/$PROJECT_FOLDER
-echo "> 필요 파일 설치 진행"
-npm install
+echo "> 디렉토리 이동 & 필요 파일 설치 진행"
+cd $REPOSITORY/$PROJECT_FOLDER && yarn install
 wait
 echo "> 빌드 시작"
-npm run start
+cd $REPOSITORY/$PROJECT_FOLDER && npm run start
